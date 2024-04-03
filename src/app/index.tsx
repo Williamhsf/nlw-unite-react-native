10812fbcc3f5516031aa4662838c5055dd46bdba
Williamhsf <williamhenriquefb@gmail.com>
2024-04-03 07:49:46 -0300

    aula 1

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text, Image } from "react-native"
+
+export default function Home(){
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <Text className="text-white">Hello React Native</Text>
+    </View>
+  )
+}
